2709d383bd6c68d01ac540bcb7d1d7bbbafa3ebc
Poltergeist <spectator.networx@gmail.com>
2014-07-17 17:25:17 +0200

    clean up code.

--- app/main.js
+++ app/main.js
@@ -2,59 +2,58 @@
 (function bookmarklet() {
   var elements = document.querySelectorAll('.issue-list-item.selected'),
     tempElement,
-    style = '\
-  body, html{\
-    height: 100%;\
-    margin: 0;\
-    padding: 0;\
-    font-size: 1.2em;\
-    font-family: sans-serif;\
-  }\
-  .card {\
-    height: 100%;\
-    width: 100%;\
-    page-break-after: always;\
-    box-sizing:border-box;\
-    margin: 0;\
-    position: relative;\
-  }\
-  .header{\
-    border-bottom: 1pt solid;\
-    padding-bottom: 6pt;\
-  }\
-  .footer {\
-    position: absolute;\
-    width: 100%;\
-    bottom: 0;\
-    border-top: 1pt solid;\
-    padding-top: 6pt;\
-  }\
-  .sub {\
-    padding-top: 6pt;\
-  }\
-  .project,\
-  .division,\
-  .complexity,\
-  .tags,\
-  .milestone,\
-  .id {\
-    display: inline-block;\
-    width: 49%;\
-  }\
-  .division,\
-  .tags,\
-  .id {\
-    width: 49%;\
-    text-align: right;\
-  }\
-  .id {\
-    font-size: 1.3em;\
-    position: absolute;\
-  }\
-  .title {\
-    font-size: 1.5em;\
-  }\
-    ',
+    style =
+    'body, html{' +
+      'height: 100%;' +
+      'margin: 0;' +
+      'padding: 0;' +
+      'font-size: 1.2em;' +
+      'font-family: sans-serif;' +
+    '}' +
+    '.card {' +
+      'height: 100%;' +
+      'width: 100%;' +
+      'page-break-after: always;' +
+      'box-sizing:border-box;' +
+      'margin: 0;' +
+      'position: relative;' +
+    '}' +
+    '.header{' +
+      'border-bottom: 1pt solid;' +
+      'padding-bottom: 6pt;' +
+    '}' +
+    '.footer {' +
+      'position: absolute;' +
+      'width: 100%;' +
+      'bottom: 0;' +
+      'border-top: 1pt solid;' +
+      'padding-top: 6pt;' +
+    '}' +
+    '.sub {' +
+      'padding-top: 6pt;' +
+    '}' +
+    '.project,' +
+    '.division,' +
+    '.complexity,' +
+    '.tags,' +
+    '.milestone,' +
+    '.id {' +
+      'display: inline-block;' +
+      'width: 49%;' +
+    '}' +
+    '.division,' +
+    '.tags,' +
+    '.id {' +
+      'width: 49%;' +
+      'text-align: right;' +
+    '}' +
+    '.id {' +
+      'font-size: 1.3em;' +
+      'position: absolute;' +
+    '}' +
+    '.title {' +
+      'font-size: 1.5em;' +
+    '}',
     labelsElement,
     labelsCounter,
     title = document.querySelector('.js-repo-home-link').innerHTML || '',
@@ -75,22 +74,22 @@
           text = html.querySelector('.js-milestone-infobar-item-wrapper .css-truncate-target');
           text = text && text.innerHTML;
           temp.milestone = text || '';
-          document.body.innerHTML += '\
-<div class="card">\
-<div class="header">\
-  <div class="project">' + title + '</div>\
-  <div class="division">' + temp.division + '</div>\
-</div> \
-<div class="sub">\
-  <div class="milestone">' + temp.milestone + '</div> \
-  <div class="id">' + temp.id + '</div>\
-</div>\
-<div class="title">'+ temp.title + '</div> \
-<div class="footer"> \
-  <div class="complexity">' + temp.complexity +'</div> \
-  <div class="tags">'+ temp.tags.join(', ') + '</div>\
-</div>\
-</div>';
+          document.body.innerHTML +=
+            '<div class="card">' +
+            '<div class="header">' +
+            '  <div class="project">' + title + '</div>' +
+            '  <div class="division">' + temp.division + '</div>' +
+            '</div> ' +
+            '<div class="sub">' +
+            '  <div class="milestone">' + temp.milestone + '</div> ' +
+            '  <div class="id">' + temp.id + '</div>' +
+            '</div>' +
+            '<div class="title">' + temp.title + '</div> ' +
+            '<div class="footer"> ' +
+            '  <div class="complexity">' + temp.complexity +'</div> ' +
+            '  <div class="tags">'+ temp.tags.join(', ') + '</div>' +
+            '</div>' +
+            '</div>';
         };
       oReq.onload = req;
       oReq.open('get', href, true);
